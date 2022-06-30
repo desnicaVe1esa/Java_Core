9f91b9faafac3f4c6061918ff7b86fb4bd5ace61
Kirill <seeraydary@gmail.com>
2022-06-30 11:52:30 +0300

    Многопоточка

---
+++ src/net_and_streams/RunThreads.java
@@ -0,0 +1,22 @@
+package net_and_streams;
+
+public class RunThreads implements Runnable{
+
+    public static void main(String[] args) {
+        RunThreads runThreads = new RunThreads();
+        Thread alfa = new Thread(runThreads);
+        Thread beta = new Thread(runThreads);
+        alfa.setName("альфа");
+        beta.setName("бета");
+        alfa.start();
+        beta.start();
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 25; i ++) {
+            String threadName = Thread.currentThread().getName();
+            System.out.println("Сейчас работает: " + threadName);
+        }
+    }
+}
