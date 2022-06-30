9bc281ad1b6596ba97fa5f5c164a010b222863a8
Kirill <seeraydary@gmail.com>
2022-06-30 08:47:56 +0300

    Многопоточка

---
+++ src/net_and_streams/TestRunnable.java
@@ -0,0 +1,32 @@
+package net_and_streams;
+
+public class TestRunnable implements Runnable {
+
+    @Override
+    public void run() {
+        go();
+    }
+
+    public void go() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        doMore();
+    }
+
+    public void doMore() {
+        System.out.println("Вершина стека");
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        Runnable runnable = new TestRunnable();
+        Thread thread = new Thread(runnable);
+        thread.start();
+
+        System.out.println("Метод main");
+    }
+}
